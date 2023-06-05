405ba0ceb38a79a7da95e6f4e680622d48d19f3a
ZAINREHMAN55 <aptechzain1@gmail.com>
2023-06-05 16:15:16 +0500

    check

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import LoginButton from './component/LoginButton';
+import LogoutButton from './component/LogoutButton';
+
+function App() {
+  return (
+    <>
+    <LoginButton/>
+    <LogoutButton/>
+    </>
+  );
+}
+
+export default App;
